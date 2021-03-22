7a6df068dea2d93703ad8e0f3464bbd99cc10f1d
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-21 17:43:06 -0700

    Feat: Followed the module, used some references to create the typeDefs. Mutation might need edits. Like calling authentication later and user I think.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,7 +4,37 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
     type Query {
-        
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!)
+        addUser(username: String!, email: String!, password: String!)
+        saveBook(input: savedBook!)
+        removeBook(bookId: ID!)
+    }
+
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [String]
+    }
+
+    type Book {
+        _id: ID!
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
 `;
 
